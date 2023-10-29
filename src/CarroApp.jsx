ad21a5619fc3ab6c6304b09a7d875f8c1f52c3c4
Rolando <r.diaz@buscalibre.com>
2023-10-29 20:37:23 -0300

    pantalla de carro basico

---
+++ src/CarroApp.jsx
@@ -0,0 +1,100 @@
+export const CarroApp = () => {
+    return (
+        <>
+            <div className="container">
+                <h1>Carro</h1>
+
+                <div className="row">
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 1</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 2</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 3</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 4</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 5</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 6</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 7</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-3 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Producto 8</h5>
+                                <p className="card-text">Descripcion del producto 1 asd del producdsasdasadsd sadsacasc</p>
+                                <p className="card-text">$50.000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { CarroApp } from './CarroApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CarroApp />
   </React.StrictMode>,
 )
